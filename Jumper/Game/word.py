bad16bb33c2d7b4507e04127d37cd09566bfa943
Colby Patterson <cpatt@byui.edu>
2022-02-05 16:20:44 -0700

    Added choose_word method to Word class

--- Jumper/Game/word.py
+++ Jumper/Game/word.py
@@ -1,3 +1,8 @@
+import random
+
 class Word:
     def __init__(self):
         self.word = ["Coach", "Apple", "Event", "Index", "Shift"]
+
+    def choose_word(self):
+        word = random.choice(self.word)
