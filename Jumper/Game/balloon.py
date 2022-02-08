3e5bed9b973a6470aa7123515f6ea3232f1e93a6
Ethan Chipman <ethandchipman@gmail.com>
2022-02-07 17:39:46 -0700

    saving for later

--- Jumper/Game/balloon.py
+++ Jumper/Game/balloon.py
@@ -1,80 +1,48 @@
+import os
+from Game.terminal_service import TerminalService
 from Game.word import Word
 class Balloon:
     
 
   def __int__(self):
 
-    self._tires = 5
+    self._tries = 5
     self._guessed_letter = []
     self._is_playing = True
+    self._word_completetion = '_' * len(self.word)
+    self._balloon_image = TerminalService()
+    self.word = Word()
+
+  def check_letters(self):
+    print(self._word_completetion)
+    for i in range (0,len(self.word)):
+      letter = self.word[i]
+      if self.guess == letter:
+        self._word_completetion[i] = self.guess
+    if '_' not in self._word_completetion:
+      return False
+    else:
+      return True
+
+  def status(self):
+    os.system("clear")
+    print(self._balloon_image.display_balloon[self.tries])
+    print(self._word_completetion)
 
   
-  def guesses(self):
+  def play(self):
 
     while self._is_playing == True and self._tries > 0:
-      guess = input('Guess a letter [a-z]: ')
-      guess = guess.lower()
+      
+      self.guess = input('Guess a letter [a-z]: ')
+      self.guess = self.guess.lower()
 
-      if guess == self.word:
+      if self.guess == self.word:
         self._is_playing = False
-      if len(guess) == 1 and guess in self.word:
+      if len(self.guess) == 1 and self.guess in self.word:
         for i in range(0,len(self.word)):
           letter = self.word[i]
-
-  def display_balloon(tries):
-    stages = [ """
-                        x
-                       /|\
-                       / \
-                     
-                     ^^^^^^^
-                   """,
-                   """
-                       \ /
-                        o
-                       /|\
-                       / \
-                     
-                     ^^^^^^^
-                   """,
-                   """
-                      \   /
-                       \ /
-                        o
-                       /|\
-                       / \
-                     
-                     ^^^^^^^
-                   """,
-                   """
-                       ___
-                      \   /
-                       \ /
-                        o
-                       /|\
-                       / \
-                     
-                     ^^^^^^^
-                   """,
-                   """
-                      /___\
-                      \   /
-                       \ /
-                        o
-                       /|\
-                       / \
-                     
-                     ^^^^^^^
-                   """,
-                   """
-                       ___
-                      /___\
-                      \   /
-                       \ /
-                        o
-                       /|\
-                       / \
-                     
-                     ^^^^^^^
-                   """]
-    return stages[tries]
+          if self.guess == letter:
+            self._word_completetion[i] = self.guess
+        if '_' not in self._word_completetion:
+          self._is_playing = False
