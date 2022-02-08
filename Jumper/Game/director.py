39be1b7941ebc3f2f391b99111dbfed25be09d00
Ethan Chipman <ethandchipman@gmail.com>
2022-02-08 16:58:30 -0700

    2nd push, it worksish

--- Jumper/Game/director.py
+++ Jumper/Game/director.py
@@ -13,29 +13,20 @@ class Director:
         self._balloon = Balloon()
         self._word = Word()
         self._terminal_service = TerminalService()
-<<<<<<< HEAD
         self._guess = ''
-=======
->>>>>>> 57b9ab5317304e9807339dbf01d54904ac167da3
 
     def start_game(self):
 
         while self._is_playing:
             self._get_inputs()
-<<<<<<< HEAD
             # self._do_updates()
             # self._do_outputs()
-=======
-            #self._do_updates()
-            #self._do_outputs()
->>>>>>> 57b9ab5317304e9807339dbf01d54904ac167da3
 
     def _get_inputs(self):
 
         new_balloon = self._balloon.status()
         self._balloon.play()
 
-<<<<<<< HEAD
 
     # def _do_updates(self):
 
@@ -45,8 +36,3 @@ class Director:
     # def _do_outputs(self):
 
     #     pass
-=======
-    #def _do_updates():
-
-        #pass
->>>>>>> 57b9ab5317304e9807339dbf01d54904ac167da3
--- Jumper/Game/word.py
+++ Jumper/Game/word.py
@@ -4,7 +4,6 @@ class Word:
 
 
     def __init__(self):
-<<<<<<< HEAD
         self.word_list = ["Coach", "Apple", "Event", "Index", "Shift"]
 
     def pick_word(self):
@@ -13,9 +12,3 @@ class Word:
 
     # def __len__(self):
     #     return len(self.word)
-=======
-        self.word = random.choice(["Coach", "Apple", "Event", "Index", "Shift"])
-
-    def __len__(self):
-        return len(self.word)
->>>>>>> 57b9ab5317304e9807339dbf01d54904ac167da3
